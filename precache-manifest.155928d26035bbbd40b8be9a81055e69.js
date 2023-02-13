79dc6464f3a24cb4f1a9fdeef280ac40f6cbb914
Deeksha <sairam7deekshas12@gmail.com>
2023-02-13 21:14:13 +0530

    Updates

---
+++ precache-manifest.155928d26035bbbd40b8be9a81055e69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccca2d12437d1569452ba9d5048aa4b8",
+    "url": "/Deeksha57/spotify-clone-reactjs/index.html"
+  },
+  {
+    "revision": "feaa9a8a6f417e1a5299",
+    "url": "/Deeksha57/spotify-clone-reactjs/static/css/main.06b2b5a6.chunk.css"
+  },
+  {
+    "revision": "35fed31d80780e7f4a13",
+    "url": "/Deeksha57/spotify-clone-reactjs/static/js/2.a26ab584.chunk.js"
+  },
+  {
+    "revision": "feaa9a8a6f417e1a5299",
+    "url": "/Deeksha57/spotify-clone-reactjs/static/js/main.1e8d460a.chunk.js"
+  },
+  {
+    "revision": "eb07e37efe00e387a888",
+    "url": "/Deeksha57/spotify-clone-reactjs/static/js/runtime-main.4578f6ab.js"
+  }
+]);
